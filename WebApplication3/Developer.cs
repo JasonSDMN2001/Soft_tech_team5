2d9185e48f9156c4c77a4b40db968dc568460062
JasonSDMN2001 <jjboi00113@gmail.com>
2022-06-30 16:36:28 +0300

    classes done

---
+++ WebApplication3/Developer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class Developer : User , UnregisteredUser
+    {
+        private String skills;
+        private String bio;
+        private Uri portfolio;
+        public Developer()
+        {
+        }
+
+        public void submitProjectOffer()
+        {
+        }
+
+        public void addProjectCategory()
+        {
+        }
+
+        public void editCancelOffer()
+        {
+        }
+
+        public void acceptProject()
+        {
+        }
+
+        public void searchProject()
+        {
+        }
+
+        public void searchDev()
+        {
+        }
+
+        public void projectDescShow()
+        {
+        }
+
+        public void profileShow()
+        {
+        }
+
+        public void profileCreate()
+        {
+        }
+
+        public void searchClient()
+        {
+        }
+    }
+}
---
+++ WebApplication3/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class Project
+    {
+        public String title;
+        public String description;
+        public Boolean type;
+        public Boolean offer_show;
+        public String category;
+        public String subcategory;
+        public String payment_method;
+        public int max_price;
+        public int interval;
+        public int uptime;
+        public String rec_tech;
+
+        public Project()
+        {
+        }
+    }
+}
