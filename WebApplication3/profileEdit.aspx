2358b7c1da9da12522885cce3b96b2f78cd03afd
Patroklos Georgiadis <efthimisgeorgiadis17@gmail.com>
2022-09-19 19:13:53 +0300

    Profile Editing Page (client)

--- WebApplication3/profileEdit.aspx
+++ WebApplication3/profileEdit.aspx
@@ -9,17 +9,17 @@
 <body height="80%">
     <form id="profileEdit" runat="server">
         <table>
-            <tr><td><asp:TextBox ID="username" runat="server" placeholder="Username" /></td></tr>
-            <tr><td><asp:TextBox ID="email" runat="server" placeholder="Email" /></td></tr>
-            <tr><td><asp:TextBox ID="firstname" runat="server" placeholder="First name:" /></td></tr>
-            <tr><td><asp:TextBox ID="lastname" runat="server" placeholder="Last name:" /></td></tr>
-            <tr><td><asp:TextBox id="gender" runat="server" placeholder="♀ ♂"/></td></tr>
-            <tr><td><asp:TextBox id="birthdate" runat="server" placeholder="Birthdate"/></td></tr>
+            <tr><td><asp:Label runat="server" Text="Username:"/></td><td><asp:TextBox ID="username" runat="server" placeholder="Username" /></td></tr>
+            <tr><td><asp:Label runat="server" Text="Email:"/></td><td><asp:TextBox ID="email" runat="server" placeholder="Email" /></td></tr>
+            <tr><td><asp:Label runat="server" Text="First name:"/></td><td><asp:TextBox ID="firstname" runat="server" placeholder="First name:" /></td></tr>
+            <tr><td><asp:Label runat="server" Text="Last name:"/></td><td><asp:TextBox ID="lastname" runat="server" placeholder="Last name:" /></td></tr>
+            <tr><td><asp:Label runat="server" Text="Gender:"/></td><td><asp:TextBox id="gender" runat="server" placeholder="♀ ♂"/></td></tr>
+            <tr><td><asp:Label runat="server" Text="Birth date:"/></td><td><asp:TextBox id="birthdate" runat="server" placeholder="Birthdate"/></td></tr>
             <tr><td width="40%"><asp:Image ID="ImageID" runat="server" Height="100px" Width="100%" /></td>
                 <td><asp:Button runat="server" Text="Insert new Image" ID="imagebtn" /></td></tr>
-            <tr><td><asp:TextBox ID="pagelink" runat="server" placeholder="example.com" /></td></tr>
-            <tr><td><asp:TextBox ID="description" runat="server" placeholder="Description" /></td></tr>
-            <tr><td><asp:Button runat="server" Text="Save changes" ID="savebtn" BackColor="#00CC00" /></td>
+            <tr><td><asp:Label runat="server" Text="Link of your page:"/></td><td><asp:TextBox ID="pagelink" runat="server" placeholder="example.com" /></td></tr>
+            <tr><td><asp:Label runat="server" Text="Your description:"/></td><td><asp:TextBox ID="description" runat="server" placeholder="Description" /></td></tr>
+            <tr><td><asp:Button runat="server" Text="Save changes" ID="savebtn" BackColor="#00CC00" OnClick="savebtn_Click" /></td>
                 <td><asp:Button runat="server" Text="Cancel" ID="cancelbtn" BackColor="Red" OnClick="cancelbtn_Click" /></td></tr>
         </table>
     </form>
--- WebApplication3/profileEdit.aspx.cs
+++ WebApplication3/profileEdit.aspx.cs
@@ -12,30 +12,57 @@ namespace WebApplication3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string user = Session["Username"].ToString();
-            SQLiteConnection conn = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
-            conn.Open();
-            String query1 = "Select * from client where username='" + user + "'";
-            SQLiteCommand cmd = new SQLiteCommand(query1, conn);
-            SQLiteDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
+            if (!Page.IsPostBack)
             {
-                email.Text = reader.GetString(0);
-                username.Text = reader.GetString(1);
-                firstname.Text = reader.GetString(3);
-                lastname.Text = reader.GetString(4);
-                gender.Text = reader.GetString(5);
-                birthdate.Text = reader.GetString(6);
-                byte[] bytes = (byte[])reader["pic"];
-                ImageID.ImageUrl = "data:image/jpg;base64," + Convert.ToBase64String(bytes);
-                description.Text = reader.GetString(8);
-                pagelink.Text = reader.GetString(9);
+                string user = Session["Username"].ToString();
+                SQLiteConnection conn = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
+                conn.Open();
+                String query1 = "Select * from client where username='" + user + "';";
+                SQLiteCommand cmd = new SQLiteCommand(query1, conn);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    email.Text = reader.GetString(0);
+                    username.Text = reader.GetString(1);
+                    firstname.Text = reader.GetString(3);
+                    lastname.Text = reader.GetString(4);
+                    gender.Text = reader.GetString(5);
+                    birthdate.Text = reader.GetString(6);
+                    byte[] bytes = (byte[])reader["pic"];
+                    ImageID.ImageUrl = "data:image/jpg;base64," + Convert.ToBase64String(bytes);
+                    description.Text = reader.GetString(8);
+                    pagelink.Text = reader.GetString(9);
+                }
+                conn.Close();
             }
-            conn.Close();
         }
 
         protected void cancelbtn_Click(object sender, EventArgs e)
         {
+            Response.Redirect("clientProfile.aspx");
+        }
+
+        protected void savebtn_Click(object sender, EventArgs e)
+        {
+            string user = Session["Username"].ToString();
+            SQLiteConnection conU = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
+            conU.Open();
+            String queryU = "UPDATE client SET username=@username, email=@email, firstname=@firstname, lastname=@lastname, "
+                + "gender=@gender, birthdate=@birthdate, pic=@pic, pagelink=@pagelink, description=@description"
+                +" where username='" + user + "';";
+            SQLiteCommand cmd = new SQLiteCommand(queryU, conU);
+            cmd.Parameters.AddWithValue("@username", username.Text);
+            cmd.Parameters.AddWithValue("@email", email.Text);
+            cmd.Parameters.AddWithValue("@firstname", firstname.Text);
+            cmd.Parameters.AddWithValue("@lastname", lastname.Text);
+            cmd.Parameters.AddWithValue("@gender", gender.Text);
+            cmd.Parameters.AddWithValue("@birthdate", birthdate.Text);
+            cmd.Parameters.AddWithValue("@pic", ImageID.ImageUrl);
+            cmd.Parameters.AddWithValue("@pagelink", pagelink.Text);
+            cmd.Parameters.AddWithValue("@description", description.Text);
+            Response.Write(firstname.Text);
+            cmd.ExecuteNonQuery();
+            conU.Close();
             Response.Redirect("clientProfile.aspx");
         }
     }
