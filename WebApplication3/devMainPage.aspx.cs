802cc24323f9d204994580f2c800045eca6df77d
Iason Boidanis <jjboi00113@gmail.com>
2022-09-20 22:56:18 +0300

    den xerw ti egine

--- WebApplication3/devMainPage.aspx.cs
+++ WebApplication3/devMainPage.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication3
 {
-    public partial class clientMainPage : System.Web.UI.Page
+    public partial class devMainPage : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -16,17 +16,7 @@ namespace WebApplication3
 
         protected void profileShow_Click(object sender, EventArgs e)
         {
-            clientframe.Src = "clientProfile.aspx";
-        }
-
-        protected void createProject_Click(object sender, EventArgs e)
-        {
-            clientframe.Src = "createProject.aspx";
-        }
-
-        protected void searchClient_Click(object sender, EventArgs e)
-        {
-
+            clientframe.Src = "devProfile.aspx";
         }
     }
 }
