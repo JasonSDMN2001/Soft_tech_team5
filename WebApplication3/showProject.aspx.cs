0b41fc55c76cd6b2cf6f1fc6eedba3e054c7dadd
michalis-vslk <mixalisvslk22@gmail.com>
2022-09-26 22:19:32 +0300

    nothing

--- WebApplication3/showProject.aspx.cs
+++ WebApplication3/showProject.aspx.cs
@@ -13,19 +13,44 @@ namespace WebApplication3
         private String titlename;
         protected void Page_Load(object sender, EventArgs e)
         {
-            //titlename = Request.Params["titlename"];
-            Project view = new Project();
-            SQLiteDataReader read = view.viewProject(titlename);
-            title.Text = read.GetString(0);
-            description.Text = read.GetString(1);
-            category.Text = read.GetString(4);
-            subcategory.Text = read.GetString(5);
-            payment_method.Text = read.GetString(6);
-            max_price.Text = read.GetString(7);
-            interval.Text = read.GetString(8);
-            rec_tech.Text = read.GetString(10);
-            client_username.Text = read.GetString(12);
-
+            titlename = Request.Params["titlename"];
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
+            conn.Open();
+            SQLiteCommand projectViewcmd = new SQLiteCommand("Select * from project where title='" + titlename + "'", conn);
+            SQLiteDataReader read = projectViewcmd.ExecuteReader();
+            while (read.Read())
+            {
+                title.Text = read.GetString(0);
+                description.Text = read.GetString(1);
+                category.Text = read.GetString(4);
+                subcategory.Text = read.GetString(5);
+                payment_method.Text = read.GetString(6);
+                string price = read.GetString(7);
+                if (price == "")
+                {
+                    max_price.Text = "Doesn't exist";
+                }
+                else
+                {
+                    max_price.Text = price;
+                }
+                interval.Text = read.GetString(8);
+                string lang = read.GetString(10);
+                if (lang == "") 
+                {
+                    rec_tech.Text = "Doesn't exist";
+                }
+                else
+                {
+                    rec_tech.Text = lang;
+                }
+                client_username.Text = "Submitted by:  " + read.GetString(12);
+                if (read.GetString(14) == "true" & read.GetString(15) == "true")
+                {
+                    complete.Visible = true;
+                }
+            }
+            conn.Close();
         }
     }
 }
