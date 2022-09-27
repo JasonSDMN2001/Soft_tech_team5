c6feb3e06b4989e0966c7267b3218a1198f69cd6
Patroklos Georgiadis <efthimisgeorgiadis17@gmail.com>
2022-09-27 23:07:50 +0300

    idk

--- WebApplication3/submitOffer.aspx.cs
+++ WebApplication3/submitOffer.aspx.cs
@@ -22,21 +22,13 @@ namespace WebApplication3
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            //title = Session["value"].ToString();
-            //user = Session["Username"].ToString();
+            
             date = DateTime.Now.ToString("dd-MM-yyyy");
-            Response.Redirect("showProject.aspx");
             SQLiteConnection conn = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
             conn.Open();
-            SQLiteCommand offerCreatecmd = new SQLiteCommand("Insert into dev_profile_hidden(email,fullname,gender,username,port,cv) Values(@email,@fullname,@gender,@username,@port,@cv)", conn);
 
-            //SQLiteCommand offerCreatecmd = new SQLiteCommand("Insert into dev_offer(username,title,sum_offer,date_offer,comments) Values(@username,@title,@sum_offer,@date_offer,@comments)", conn);
+            SQLiteCommand offerCreatecmd = new SQLiteCommand("Insert into dev_offer(username,title,sum_offer,date_offer,comments) Values(@username,@title,@sum_offer,@date_offer,@comments)", conn);
             offerCreatecmd.Parameters.AddWithValue("@username", TextBox3.Text);
-            /*offerCreatecmd.Parameters.AddWithValue("@email", "hey");
-            offerCreatecmd.Parameters.AddWithValue("@fullname", "hey");
-            offerCreatecmd.Parameters.AddWithValue("@gender", "hey");
-            offerCreatecmd.Parameters.AddWithValue("@port", "hey");
-            offerCreatecmd.Parameters.AddWithValue("@cv", "hey");*/
             offerCreatecmd.Parameters.AddWithValue("@title", TextBox4.Text);
             offerCreatecmd.Parameters.AddWithValue("@sum_offer", TextBox1.Text);
             offerCreatecmd.Parameters.AddWithValue("@date_offer", date);
